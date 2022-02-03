4cde8d720f2093b731135787266f4ed1e646e372
brandonmizutani <bran2miz@gmail.com>
2022-02-02 20:23:35 -0800

    add

--- api/get_request.py
+++ api/get_request.py
@@ -10,17 +10,15 @@ class handler(BaseHTTPRequestHandler):
     query_string_list = parse.parse_qsl(url_components.query)
     dic = dict(query_string_list)
 
-    if "post code" in dic:
-        url = 'https://api.zippopotam.us/'
-        r = requests.get(url + dic['post code'])
-        data = r.json()
-        postal_code = []
-        for zip_data in data:
-            place_info = zip_data["places"][0]["place name"]
-            postal_code.append(place_info)
-        message = str(postal_code)        
-    else:
-        message = "Please give me a postal code to find"
+    url = 'https://api.zippopotam.us/'
+    r = requests.get(url + dic['post code'])
+    data = r.json()
+    postal_code = []
+    for zip_data in data:
+      place_info = zip_data["places"][0]["place name"]
+      postal_code.append(place_info)
+        # message = str(postal_code)        
+        # message = "Please give me a postal code to find"
 
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
